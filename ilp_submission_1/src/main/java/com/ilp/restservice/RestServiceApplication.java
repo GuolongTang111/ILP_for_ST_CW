bd99310f04bc222fa39258d1cbcf401038f8f167
Guolong <gtang2@ed.ac.uk>
2025-01-21 01:37:06 +0000

    test CI function

--- ilp_submission_1/src/main/java/com/ilp/restservice/RestServiceApplication.java
+++ ilp_submission_1/src/main/java/com/ilp/restservice/RestServiceApplication.java
@@ -12,7 +12,7 @@ public class RestServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestServiceApplication.class, args);
 	}
-	private static final String id = "s2286997";
+	private static final String id = "s2286997@ed.ac.uk";
 	@GetMapping("/uuid")
     public String uuid() {
       	return id;
