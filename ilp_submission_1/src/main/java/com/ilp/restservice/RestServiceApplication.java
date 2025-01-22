7a924c040ff59792fc7b39b92fa6e6f05f895d8c
Guolong <gtang2@ed.ac.uk>
2025-01-22 14:32:00 +0000

    adding system test

--- ilp_submission_1/src/main/java/com/ilp/restservice/RestServiceApplication.java
+++ ilp_submission_1/src/main/java/com/ilp/restservice/RestServiceApplication.java
@@ -12,7 +12,7 @@ public class RestServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestServiceApplication.class, args);
 	}
-	private static final String id = "s11111111";
+	private static final String id = "s2286997";
 	@GetMapping("/uuid")
     public String uuid() {
       	return id;
